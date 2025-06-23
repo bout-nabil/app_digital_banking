44cbb30d249188750325ddd28c01a77c8a526731
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-06-23 10:58:03 +0100

    Work on Debit and Credit an Account, Handling exceptions

--- src/main/java/ma/enset/ebankbackend/services/IBankAccountService.java
+++ src/main/java/ma/enset/ebankbackend/services/IBankAccountService.java
@@ -1,16 +1,22 @@
 package ma.enset.ebankbackend.services;
 
 import ma.enset.ebankbackend.entities.BankAccount;
+import ma.enset.ebankbackend.entities.CurrentAccount;
 import ma.enset.ebankbackend.entities.Customer;
+import ma.enset.ebankbackend.entities.SavingAccount;
+import ma.enset.ebankbackend.exceptions.BalanceNotSufficientException;
+import ma.enset.ebankbackend.exceptions.BankAccountNotFoundException;
+import ma.enset.ebankbackend.exceptions.CustomerNotFoundException;
 
 import java.util.List;
 
 public interface IBankAccountService {
     Customer saveCustomer(Customer customer);
-    BankAccount saveBankAccount(double initialBalance, String typeAccount, Long customerId);
+    CurrentAccount saveCurrentBankAccount(double initialBalance, Long customerId, double overDraft) throws CustomerNotFoundException;
+    SavingAccount saveSavingBankAccount(double initialBalance, Long customerId, double interestRate) throws CustomerNotFoundException;
     List<Customer> CUSTOMER_LIST();
-    BankAccount getBankAccount(String accountId);
-    void debit(String accountId, double amount, String description);
-    void credit(String accountId, double amount, String description);
-    void tranfer(String accountIdSource, String accountIdDestination, double amount);
+    BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException;
+    void debit(String accountId, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
+    void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
+    void tranfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
 }
