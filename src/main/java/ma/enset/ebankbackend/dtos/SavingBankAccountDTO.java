59615049fc89263abcbcaa2bed363ef800984742
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-06-24 13:24:18 +0100

    Work on BankAccountRestAPI

---
+++ src/main/java/ma/enset/ebankbackend/dtos/SavingBankAccountDTO.java
@@ -0,0 +1,16 @@
+package ma.enset.ebankbackend.dtos;
+
+import lombok.Data;
+import ma.enset.ebankbackend.Enums.AccountStatus;
+import java.util.Date;
+
+@Data
+public class SavingBankAccountDTO extends BankAccountDTO {
+    private String idBankAccount;
+    private double balance;
+    private Date CreatedAccount;
+    private AccountStatus accountStatus;
+    private String currency;
+    private CustomerDTO customerDTO;
+    private double interestRate;
+}
--- src/main/java/ma/enset/ebankbackend/services/IBankAccountService.java
+++ src/main/java/ma/enset/ebankbackend/services/IBankAccountService.java
@@ -1,6 +1,9 @@
 package ma.enset.ebankbackend.services;
 
+import ma.enset.ebankbackend.dtos.BankAccountDTO;
+import ma.enset.ebankbackend.dtos.CurrentBankAccountDTO;
 import ma.enset.ebankbackend.dtos.CustomerDTO;
+import ma.enset.ebankbackend.dtos.SavingBankAccountDTO;
 import ma.enset.ebankbackend.entities.BankAccount;
 import ma.enset.ebankbackend.entities.CurrentAccount;
 import ma.enset.ebankbackend.entities.Customer;
@@ -13,10 +16,10 @@ import java.util.List;
 
 public interface IBankAccountService {
     CustomerDTO saveCustomer(CustomerDTO customerDTO);
-    CurrentAccount saveCurrentBankAccount(double initialBalance, Long customerId, double overDraft) throws CustomerNotFoundException;
-    SavingAccount saveSavingBankAccount(double initialBalance, Long customerId, double interestRate) throws CustomerNotFoundException;
+    CurrentBankAccountDTO saveCurrentBankAccount(double initialBalance, Long customerId, double overDraft) throws CustomerNotFoundException;
+    SavingBankAccountDTO saveSavingBankAccount(double initialBalance, Long customerId, double interestRate) throws CustomerNotFoundException;
     List<CustomerDTO> CUSTOMER_LIST();
-    BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException;
+    BankAccountDTO getBankAccount(String accountId) throws BankAccountNotFoundException;
     void debit(String accountId, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
     void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
     void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
